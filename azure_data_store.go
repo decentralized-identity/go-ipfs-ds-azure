9a4c7212fc2ffda80296c628ba8f453f8fc66028
Isaac Chen <ische@microsoft.com>
2020-08-05 13:45:37 -0700

    use default behaviors

--- azure_data_store.go
+++ azure_data_store.go
@@ -6,25 +6,12 @@ import (
 	"log"
 	"context"
 	"os"
-	"strings"
 	"bytes"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
 
 	ds "github.com/ipfs/go-datastore"
 	dsq "github.com/ipfs/go-datastore/query"
-)
-
-const (
-	// listMax is the largest amount of objects you can request from S3 in a list
-	// call.
-	listMax = 1000
-
-	// deleteMax is the largest amount of objects you can delete from S3 in a
-	// delete objects call.
-	deleteMax = 1000
-
-	defaultWorkers = 100
 )
 
 // AzureStorage is a storage representation
@@ -195,28 +182,12 @@ func (storage *AzureStorage) Delete(k ds.Key) error {
 		return nil
 }
 
-func (s *AzureStorage) Query(q dsq.Query) (dsq.Results, error) {
-}
-
-func (s *AzureStorage) Batch() (ds.Batch, error) {
-	return &AzureBatch{}, nil
+// Query returns a dsq result
+func (storage *AzureStorage) Query(q dsq.Query) (dsq.Results, error) {
+	return nil, fmt.Errorf("Azure storage query not supported")
 }
 
 // Close is not implemented
 func (storage *AzureStorage) Close() error {
 	return nil
 }
-
-type AzureBatch struct {
-}
-
-func (b *AzureBatch) Put(k ds.Key, val []byte) error {
-}
-
-func (b *AzureBatch) Delete(k ds.Key) error {
-}
-
-func (b *AzureBatch) Commit() error {
-}
-
-var _ ds.Batching = (*AzureStorage)(nil)
